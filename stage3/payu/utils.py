8b55cfdef8b200b2617d66073f5541af9383139f
venus maroon <venus maroon>
2016-09-25 04:21:15 +0530

    Final Easy Returns

---
+++ stage3/payu/utils.py
@@ -0,0 +1,24 @@
+from hashlib import sha512
+from django.conf import settings
+KEYS = ('key', 'txnid', 'amount', 'productinfo', 'firstname', 'email',
+        'udf1', 'udf2', 'udf3', 'udf4', 'udf5',  'udf6',  'udf7', 'udf8',
+        'udf9',  'udf10')
+
+def generate_hash(data):
+    keys = ('key', 'txnid', 'amount', 'productinfo', 'firstname', 'email',
+            'udf1', 'udf2', 'udf3', 'udf4', 'udf5',  'udf6',  'udf7', 'udf8',
+            'udf9',  'udf10')
+    hash = sha512('')
+    for key in KEYS:
+        hash.update("%s%s" % (str(data.get(key, '')), '|'))
+    hash.update(settings.PAYU_INFO.get('merchant_salt'))
+    return hash.hexdigest().lower()
+
+def verify_hash(data):
+    KEYS_REVERSED = KEYS[::-1]
+    hash = sha512('')
+    hash.update(settings.PAYU_INFO.get('merchant_salt'))
+    hash.update("%s%s" % ('|', str(data.get('status', ''))))
+    for key in KEYS_REVERSED:
+        hash.update("%s%s" % ('|', str(data.get(key, ''))))
+    return hash.hexdigest().lower() == str(data.get('hash', ''))
